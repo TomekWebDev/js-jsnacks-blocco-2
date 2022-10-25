58a8d7602280651a0d7ea6b968df175fd0fbee50
TomekWebDev <tommaso.mangiagalli@gmail.com>
2022-10-25 15:24:45 +0200

    esercizio 2 versione star wars fatto

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 // JSnacks
 
-// Esercizio 1
+// // Esercizio 1
 
-let numero = parseInt(prompt("Inserisci un numero"))
+// let numero = parseInt(prompt("Inserisci un numero"))
 
-if(numero % 2 == 0){
-    console.log(numero);
-}
-else{
-    console.log((numero + 1));
-}
+// if(numero % 2 == 0){
+//     console.log(numero);
+// }
+// else{
+//     console.log((numero + 1));
+// }
+
+
+// Esercizio 2
+
+let nomi = ["Obi-Wan", "Luke", "Han", "Jar-Jar", "C3", "Jabba", "Padmè", "Qui-Gon"];
+
+let cognomi = ["Kenobi", "Skywalker", "Solo", "Binks", "P8", "The Hut", "Amidala", "Jinn"]
+
+let nomeRandom = Math.floor(Math.random() * nomi.length);
+let cognomeRandom = Math.floor(Math.random() * cognomi.length);
+
+
+console.log((nomeRandom, nomi[nomeRandom]) + " " + (cognomeRandom, cognomi[cognomeRandom]));
+
