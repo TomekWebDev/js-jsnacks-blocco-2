bb699292576b9e3a7854375e5b301166f21a5274
TomekWebDev <tommaso.mangiagalli@gmail.com>
2022-10-25 15:55:28 +0200

    esercizio 3 fatto

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // JSnacks
 
+
 // // Esercizio 1
 
 // let numero = parseInt(prompt("Inserisci un numero"))
@@ -12,15 +13,32 @@
 // }
 
 
-// Esercizio 2
+// // Esercizio 2
 
-let nomi = ["Obi-Wan", "Luke", "Han", "Jar-Jar", "C3", "Jabba", "Padmè", "Qui-Gon"];
+// let nomi = ["Obi-Wan", "Luke", "Han", "Jar-Jar", "C3", "Jabba", "Padmè", "Qui-Gon"];
 
-let cognomi = ["Kenobi", "Skywalker", "Solo", "Binks", "P8", "The Hut", "Amidala", "Jinn"]
+// let cognomi = ["Kenobi", "Skywalker", "Solo", "Binks", "P8", "The Hut", "Amidala", "Jinn"]
 
-let nomeRandom = Math.floor(Math.random() * nomi.length);
-let cognomeRandom = Math.floor(Math.random() * cognomi.length);
+// let nomeRandom = Math.floor(Math.random() * nomi.length);
+// let cognomeRandom = Math.floor(Math.random() * cognomi.length);
 
 
-console.log((nomeRandom, nomi[nomeRandom]) + " " + (cognomeRandom, cognomi[cognomeRandom]));
+// console.log((nomeRandom, nomi[nomeRandom]) + " " + (cognomeRandom, cognomi[cognomeRandom]));
 
+
+// Esercizio 3
+
+// for (i=0; i < 20; i++){
+
+// }
+
+let numeri = ["3", "1", "3", "1", "3", "1", "3", "1", "3"]
+
+let somma = 0;
+
+for(i = 1; i < numeri.length; i += 2){
+
+        somma += parseInt(numeri[i])
+}
+
+console.log(somma);
